1952ffca07c4ae3d3f12fd4dfcdfc5924780ad60
Нормуминов Руслан <RilPax@yandex.ru>
2025-07-19 23:13:51 +0500

    style-update

--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { X, Check, Trash } from 'lucide-vue-next'
+
 defineProps<{
   todo: {
     title: string
@@ -7,25 +9,56 @@ defineProps<{
 }>()
 
 const emit = defineEmits<{
-    (e: 'toggle'): void,
-    (e: 'delete'): void
+  (e: 'toggle'): void
+  (e: 'delete'): void
 }>()
 
 const handleTodo = () => {
-    emit('toggle')
+  emit('toggle')
 }
 const HandleDelete = () => {
-    emit('delete')
+  emit('delete')
 }
-
 </script>
 
 <template>
   <div class="todo">
-    <span :class="['title', { completed: !todo.isCompleted }]">{{ todo.title }}</span>
-    <button type="button" class="button" @click="handleTodo"></button>
-    <button type="button" @click="HandleDelete"></button>
+    <span :class="['title', { completed: todo.isCompleted }]">{{
+      todo.title
+    }}</span>
+    <div class="buttons-container">
+      <button type="button" class="button" @click="handleTodo">
+        <X v-if="todo.isCompleted" :size="24" />
+        <Check v-else :size="24" />
+      </button>
+      <button type="button" class="button" @click="HandleDelete">
+        <Trash :size="24" />
+      </button>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todo {
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid var(--color-border);
+  border-radius: 15px;
+  align-items: center;
+  padding: 5px 5px;
+}
+
+.buttons-container {
+  display: flex;
+  gap: 5px;
+}
+
+.button {
+  background-color: transparent;
+  border: none;
+}
+
+.completed {
+  color: var(--color-border);
+}
+</style>
